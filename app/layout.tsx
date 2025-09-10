d732373832ecabefdf7a1f12a9349d5497663cf8
Muhammad Moiz <muhammad.moiz.26@dartmouth.edu>
2025-09-10 12:58:19 -0400

    Update

--- app/layout.tsx
+++ app/layout.tsx
@@ -50,14 +50,6 @@ export const metadata: Metadata = {
     type: "website",
     siteName: "Muhammad Moiz",
     url: "https://www.moizofficial.com/",
-    images: [
-      {
-        url: "/images/profile.jpg",
-        width: 1200,
-        height: 630,
-        alt: "Muhammad Moiz headshot",
-      },
-    ],
   },
   twitter: {
     card: "summary_large_image",
